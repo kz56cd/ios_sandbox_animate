cbbf8347bc284fb2866443074ebfd0c346232fd2
kz56cd <kz56cd@gmail.com>
2016-03-12 17:51:14 +0900

    add xc project and pods

---
+++ IosSandboxAnimate/IosSandboxAnimate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IosSandboxAnimate
+//
+//  Created by msano on 2016/03/12.
+//  Copyright © 2016年 msano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
